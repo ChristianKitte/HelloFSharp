61876621eded791026bb6d6f25f6c03e60445bab
Christian Kitte <chkitte@web.de>
2021-01-13 19:58:09 +0100

    Projektdateien hinzufügen.

---
+++ HelloFSharp/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let ausgabe1="Hello World from F#!"
+let ausgabe2 eingabeVonKonsole="Ihre Eingabe " + eingabeVonKonsole
+
+[<EntryPoint>]
+let main argv =                    
+    Console.WriteLine(ausgabe1)   
+    Console.WriteLine(ausgabe2(Console.ReadLine()))
+    0 // return an integer exit code
+
