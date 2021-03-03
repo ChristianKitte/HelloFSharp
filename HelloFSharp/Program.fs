f926d9758b3a50eabdb825a5ff5bb069ef126a33
Christian Kitte <christian.kitte@stud.hs-emden-leer.de>
2021-03-03 10:30:20 +0100

    Kleine Anpassungen und Kommentare

--- HelloFSharp/Program.fs
+++ HelloFSharp/Program.fs
@@ -1,11 +1,17 @@
 ﻿open System
 
-let ausgabe1="Hello World from F#!"
-let ausgabe2 eingabeVonKonsole="Ihre Eingabe " + eingabeVonKonsole
+// Eine Variable/Funktion, welche ein String defniert
+let ausgabe1="Hello World from F#! Geben Sie etwas ein und drücken Sie ENTER:"
+// Eine Funktion "ausgabe2" mit einem Parameter "eingabeVonKonsole"
+let ausgabe2 eingabeVonKonsole="Ihre Eingabe : " + eingabeVonKonsole
 
 [<EntryPoint>]
 let main argv =                    
     Console.WriteLine(ausgabe1)   
+    
+    // Der MEthode "WriteLine" wird die Methode "ausgabe2" übergeben. Dieser wird als Parameter die Funktion "ReadLine"
+    // übergeben
     Console.WriteLine(ausgabe2(Console.ReadLine()))
+
     0 // return an integer exit code
 
